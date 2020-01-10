67c1ed23a2074c16d678eeb37c50654bebdebb7a
nh060897 <nh060897@chichester.esirem-ad.ad.u-bourgogne.fr>
2020-01-10 15:33:40 +0100

    convertForwardTest

--- src/test/java/enigma/RotorTest.java
+++ src/test/java/enigma/RotorTest.java
@@ -6,9 +6,14 @@ import org.junit.Test;
 
 public class RotorTest {
 
+	Rotor rotor1 = Rotor.rotorFactory("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z", "E");
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void convertForwardTest() {
+		int i = 2;
+		int result = rotor1.convertForward(i);
+		int expectedResult = 2;
+		assertEquals(expectedResult, result);
 	}
-
+	
 }
